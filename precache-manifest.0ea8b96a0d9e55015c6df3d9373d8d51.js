ddada6c176d3d776bc2e7c962f460710ed46cb6e
foresightJunyu <373034891@qq.com>
2021-10-20 15:45:20 +0800

    deploy

---
+++ precache-manifest.0ea8b96a0d9e55015c6df3d9373d8d51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c26e115502547c7cec6",
+    "url": "/website-momoney/css/app.ee82adc8.css"
+  },
+  {
+    "revision": "c9b3e871d0e5061a3a831a937241ab5d",
+    "url": "/website-momoney/index.html"
+  },
+  {
+    "revision": "5c26e115502547c7cec6",
+    "url": "/website-momoney/js/app.e558548e.js"
+  },
+  {
+    "revision": "69b2d08a8362548b8a1a",
+    "url": "/website-momoney/js/chunk-vendors.cf20785d.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/website-momoney/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/website-momoney/robots.txt"
+  }
+]);
